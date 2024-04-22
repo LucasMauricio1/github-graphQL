fe6b09a29eec8ee3d6f09491b0d742f45bf2b5b9
lucasmauricio1 <lucasmauriciofc@gmail.com>
2024-04-22 12:41:00 -0300

    feat: header component

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,13 @@
+interface HeaderProps {
+  title: string
+  subtitle: string
+}
+
+export function Header({ title, subtitle }: HeaderProps) {
+  return (
+    <div className="flex flex-col items-center justify-center p-4">
+      <h1 className="text-2xl font-bold text-white">{title}</h1>
+      <p className="text-gray-400">{subtitle}</p>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Header } from './components/Header'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <Header
+        title="Repositórios no GitHub"
+        subtitle="Procure por repositórios de qualquer desenvolvedor do mundo no GitHub"
+      />
+    </main>
+  )
 }
